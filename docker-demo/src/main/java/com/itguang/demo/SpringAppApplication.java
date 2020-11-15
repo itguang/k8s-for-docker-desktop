aee688f8de9da502db9e51dfceb6a4ffaed4f620
itguang <1445037803@qq.com>
2020-11-15 10:17:30 +0800

    k8s

--- docker-demo/src/main/java/com/itguang/demo/SpringAppApplication.java
+++ docker-demo/src/main/java/com/itguang/demo/SpringAppApplication.java
@@ -10,4 +10,6 @@ public class SpringAppApplication {
         SpringApplication.run(SpringAppApplication.class, args);
     }
 
+
+
 }
--- kubia/index.js
+++ kubia/index.js
@@ -4,8 +4,8 @@ console.log("Kubia Server is Starting ...");
 
 var handler = function (request,response){
     console.log("Received message from "+ request.connection.remoteAddress);
-    response.writeHead(200);
-    response.end("You've Hit "+os.name+"\n");
+    response.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+    response.end("你现在访问的主机是： "+os.hostname()+"\n");
 }
 
 http.createServer(handler).listen(8080);
